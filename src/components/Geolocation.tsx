301447d7f53f50cfc9c4977e2f0670a380e220dc
konstantine811 <galystyan777@gmail.com>
2024-10-22 17:21:08 +0300

    Refactor Geolocation component to use GeolocationCoordinates and handle errors

--- src/components/Geolocation.tsx
+++ src/components/Geolocation.tsx
@@ -9,51 +9,47 @@ enum SourceNames {
   GpsPoint = "gps-point",
 }
 const GeolocationExample = ({ map }: Props) => {
-  const [location, setLocation] = useState<{
-    latitude: number | null;
-    longitude: number | null;
-    error: string | null;
-  }>({
-    latitude: null,
-    longitude: null,
-    error: null,
-  });
+  const [location, setLocation] = useState<GeolocationCoordinates | null>(null);
+  const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
     if (map.getSource(SourceNames.GpsPoint)) return;
-    map.addSource(SourceNames.GpsPoint, {
-      type: "geojson",
-      data: {
-        type: "FeatureCollection",
-        features: [
-          {
-            type: "Feature",
-            geometry: {
-              type: "Point",
-              coordinates: [location.longitude || 0, location.latitude || 0],
+    if (location) {
+      map.addSource(SourceNames.GpsPoint, {
+        type: "geojson",
+        data: {
+          type: "FeatureCollection",
+          features: [
+            {
+              type: "Feature",
+              geometry: {
+                type: "Point",
+                coordinates: [location.longitude || 0, location.latitude || 0],
+              },
+              properties: {},
             },
-            properties: {},
-          },
-        ],
-      },
-    });
+          ],
+        },
+      });
 
-    // Додаємо шар для точки
-    map.addLayer({
-      id: "gps-point-layer",
-      type: "circle",
-      source: "gps-point",
-      paint: {
-        "circle-radius": 10,
-        "circle-color": "#ff0000", // Червона точка
-      },
-    });
+      // Додаємо шар для точки
+      map.addLayer({
+        id: "gps-point-layer",
+        type: "circle",
+        source: "gps-point",
+        paint: {
+          "circle-radius": 10,
+          "circle-color": "#ff0000", // Червона точка
+        },
+      });
+    }
   }, [map, location]);
 
   useEffect(() => {
     // Функція для обробки успішного отримання позиції
     const handlePositionUpdate: PositionCallback = (position) => {
       const { latitude, longitude } = position.coords;
+      console.log("position", position);
       // Оновлюємо джерело даних для нових координат
       const source = map.getSource(
         SourceNames.GpsPoint
@@ -81,51 +77,46 @@ const GeolocationExample = ({ map }: Props) => {
         bearing: 45, // Поворот
         speed: 1.2, // Швидкість руху камери
       });
-      setLocation({
-        latitude,
-        longitude,
-        error: null,
-      });
+      setLocation(position.coords);
     };
 
     // Функція для обробки помилок
     const handleError: PositionErrorCallback = (error) => {
-      setLocation({
-        latitude: null,
-        longitude: null,
-        error: error.message,
-      });
+      setLocation(null);
+      setError(error.message);
     };
 
     if (navigator.geolocation) {
       // Використовуємо watchPosition для отримання оновлень у реальному часі
       navigator.geolocation.watchPosition(handlePositionUpdate, handleError, {
-        enableHighAccuracy: true, // Висока точність
-        timeout: 500, // Максимальний час очікування
-        maximumAge: 0, // Не використовувати кешовані дані
+        enableHighAccuracy: false, // Висока точність
+        timeout: 5000, // Максимальний час очікування
+        maximumAge: 0, // Максимальний вік даних
       });
     } else {
-      setLocation({
-        latitude: null,
-        longitude: null,
-        error: "Geolocation is not supported by this browser.",
-      });
+      setLocation(null);
+      setError("Geolocation is not supported by this browser.");
     }
 
     // Очищуємо стеження за позицією при розмонтуванні компонента
-  }, []);
+  }, [map]);
 
   return (
     <div>
       <h2>GPS Position</h2>
-      {location.latitude && location.longitude ? (
+      {location && location.latitude && location.longitude ? (
         <div>
+          <p>Точність {location.accuracy}м</p>
           <p>Latitude: {location.latitude}</p>
           <p>Longitude: {location.longitude}</p>
+          <p>Altitude: {location.altitude}</p>
+          <p>Altitude Accuracy: {location.altitudeAccuracy}</p>
+          <p>Heading: {location.heading}</p>
+          <p>Speed: {location.speed}</p>
         </div>
       ) : (
         <p className="text-red-400">
-          {location.error ? location.error : "No location available"}
+          {error ? error : "No location available"}
         </p>
       )}
     </div>
