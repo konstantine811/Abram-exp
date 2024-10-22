77a2093533310f58144600718fe6f645bad3f5c2
konstantine811 <galystyan777@gmail.com>
2024-10-22 12:38:54 +0300

    Add @vercel/analytics package and integrate Analytics component in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { NextUIProvider } from "@nextui-org/system";
+import { Analytics } from "@vercel/analytics/react";
 
 import "mapbox-gl/dist/mapbox-gl.css";
 import "@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css";
@@ -11,6 +12,7 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <NextUIProvider>
       <App />
+      <Analytics />
     </NextUIProvider>
   </StrictMode>
 );
