dcdd5497d44640446242b44fcd36a6578960231b
konstantine811 <galystyan777@gmail.com>
2024-10-23 17:13:22 +0300

    Refactor ThreeMap component to update Leva controls for cellSize, gap, maxHeight, and buildMaxHeight Refactor ThreeMap component to update cellSize for population normalization Refactor ThreeMap component and update label styles Add ukraineBounds to map configuration

---
+++ src/config/map.ts
@@ -0,0 +1,6 @@
+import { LngLatBoundsLike } from "mapbox-gl";
+
+export const ukraineBounds: LngLatBoundsLike = [
+  [22.137158, 44.390415], // Південно-західний кут (лівий нижній)
+  [40.22758, 52.379581], // Північно-східний кут (правий верхній)
+];
--- src/pages/ThreeMap.tsx
+++ src/pages/ThreeMap.tsx
@@ -1,7 +1,8 @@
-import { useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import mapboxgl, { Map } from "mapbox-gl";
 import usePopuplationHistogram from "@/hooks/map-hooks/usePopulationHistogram";
 import ListPlace from "@/components/map-ui/list-place";
+import { ukraineBounds } from "@/config/map";
 
 // Helper function to create square polygons
 
@@ -11,6 +12,47 @@ const ThreeMap = () => {
   const [map, setMap] = useState<Map>();
 
   const { loading, places } = usePopuplationHistogram({ map });
+
+  const orbitAngle = useRef(0);
+  const isAnimatingRef = useRef(true); // Зберігаємо стан анімації через useRef
+  const animationId = useRef<number | null>(null); // Зберігаємо ID анімації для можливого зупинення
+
+  useEffect(() => {
+    if (places && places.length && map) {
+      isAnimatingRef.current = false; // Зупиняємо анімацію
+      setTimeout(() => {
+        map.fitBounds(ukraineBounds, {
+          padding: { top: 10, bottom: 10, left: 10, right: 10 }, // Додає відступи для меж карти
+        });
+      }, 300);
+    }
+  }, [places, map]);
+
+  const animateOrbit = useCallback(
+    (map: Map) => {
+      const radius = 30; // Радіус орбіти
+      const centerLng = 0; // Довгота центру орбіти
+      const centerLat = 0; // Широта центру орбіти
+
+      // Зміна кута для обертання по орбіті
+      orbitAngle.current += 0.0002;
+
+      // Обчислення нових координат на основі кута орбіти
+      const lng = centerLng + radius * Math.cos(orbitAngle.current);
+      const lat = centerLat + radius * Math.sin(orbitAngle.current);
+
+      // Оновлення положення камери
+      map.setCenter([lng, lat]);
+      map.setBearing(orbitAngle.current * (180 / Math.PI));
+      map.setPitch(60);
+
+      // Запланувати наступний кадр анімації, якщо вона активна
+      if (isAnimatingRef.current) {
+        animationId.current = requestAnimationFrame(() => animateOrbit(map));
+      }
+    },
+    [isAnimatingRef]
+  );
 
   useEffect(() => {
     mapboxgl.accessToken =
@@ -24,14 +66,17 @@ const ThreeMap = () => {
       });
       mapRef.current.on("load", () => {
         setMap(mapRef.current);
+        if (mapRef.current) {
+          animateOrbit(mapRef.current);
+        }
       });
     }
-  }, []);
+  }, [animateOrbit]);
   return (
     <div className="min-h-screen relative">
       <div className="min-h-screen relative z-10" ref={mapContainerRef} />
       {loading && (
-        <div className="fixed top-0 w-full h-full z-[10000] bg-black/15 backdrop-blur-sm flex justify-center items-center text-black">
+        <div className="fixed top-0 pointer-events-none w-full h-full z-[10000] bg-black/5  flex justify-center items-center text-black">
           Please wait, Loading a data
         </div>
       )}
